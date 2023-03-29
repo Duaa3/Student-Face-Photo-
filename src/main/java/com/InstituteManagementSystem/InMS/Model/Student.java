a118df0d52694982da69d67b578e3cf9414343d6
Duaa3 <Duaahil44@gmail.com>
2023-03-29 07:31:57 +0400

    first commit

--- src/main/java/com/InstituteManagementSystem/InMS/Model/Student.java
+++ src/main/java/com/InstituteManagementSystem/InMS/Model/Student.java
@@ -1,8 +1,10 @@
-package com.InstituteManagementSystem.InMS.Model;
+package Model;
 // Defining Student class with three public instance variables
+
 public class Student {
     public int id;
 
     public String name;
     public String email;
+
 }
